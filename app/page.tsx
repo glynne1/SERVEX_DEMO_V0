1fcd8f5b068464023a5018640f5e10a90fe47273
glynnethec <alexglynne7@gmail.com>
2025-12-07 16:17:00 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,53 @@
 import Image from "next/image";
+import Main1 from './components/main1'
+import GLYNNEMatrix from "./components/menuIicial"; // Ajusta la ruta si tu componente está en otra carpeta
+import Header from "./components/header";
+import CardM from './components/cardMarcketinng'
+
+import CData from './components/CarddData'
+import Movile from './components/movileCard'
+import Footer from './components/footer'
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="w-full min-h-screen bg-white text-black">
+      {/* Header fijo */}
+      <Header />
+
+    
+      {/* Contenido principal */}
+      <main
+  className="w-full pt-14 bg-black text-white bg-cover bg-center"
+  style={{ backgroundImage: "url('https://i.pinimg.com/736x/6e/79/0f/6e790fbd1a0166e5b40338d024628330.jpg')" }}
+>
+  <GLYNNEMatrix />
+</main>
+
+
+ {/* Contenido principal */}
+ <main className="w-full pt-14 bg-white text-black">
+        {/* pt-14 = padding top igual a la altura del header */}
+        <CardM/>
+      </main>
+
+        
+      
+
+      <main className="w-full pt-14 bg-white text-black">
+        {/* pt-14 = padding top igual a la altura del header */}
+        <CData />
+      </main>
+      
+  
+        
+      <main className="w-full pt-14 bg-white text-black">
+        {/* pt-14 = padding top igual a la altura del header */}
+        <Movile />
+      </main>
+      <main className="w-full pt-14 bg-white text-black">
+        {/* pt-14 = padding top igual a la altura del header */}
+        <Footer />
       </main>
     </div>
   );
